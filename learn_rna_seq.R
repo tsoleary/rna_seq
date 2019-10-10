869348e658c6f926a9b185aadb72519a462a34c3
tsoleary <tsoleary12@gmail.com>
2019-10-10 17:16:07 -0400

    starting to learn the workflow

---
+++ learn_rna_seq.R
@@ -0,0 +1,15 @@
+# example gene count data ----
+
+https://bioc.ism.ac.jp/packages/2.14/bioc/vignettes/DESeq2/inst/doc/beginner.pdf
+
+library("DESeq2")
+# BiocManager::install("parathyroidSE")
+library("parathyroidSE")
+
+data("parathyroidGenesSE")
+se <- parathyroidGenesSE
+colnames(se) <- se$run
+
+
+library(PIVOT)
+pivot()
