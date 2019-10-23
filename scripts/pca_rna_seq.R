266b521e5bae8f7f49b2dec2c71be8a0c978257f
tsoleary <tsoleary12@gmail.com>
2019-10-23 10:26:07 -0400

    organized

--- scripts/pca_rna_seq.R
+++ scripts/pca_rna_seq.R
@@ -3,6 +3,8 @@
 # read in the normalized counts .csv file (obtained from PIVOT/DESeq norm)
 norm_counts <- read.csv("/Users/tsoleary/R/rna_seq/data/dm_counts_norm.csv")
 
+outputPrefix <- "Dm_DESeq2"
+
 # matrix of values
 data.matrix <- as.matrix(norm_counts[, -1])
 
@@ -38,28 +40,35 @@ ggplot(data=pca.data, aes(x=X, y=Y, label=Sample)) +
   theme_bw() +
   ggtitle("PCA")
 
+dev.copy(png, paste0(outputPrefix, "_pca_text.png"))
+dev.off()
+
 # plot colored by group
 ggplot(data = pca.data, aes(x = X, y = Y, fill = group)) +
-  geom_point(pch = 21, size =5) +
+  geom_point(pch = 21, size = 5) +
+  scale_fill_manual(values=c("skyblue", "#999999", "firebrick1")) +
   xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
   ylab(paste("PC2 - ", pca.var.per[2], "%", sep="")) +
   theme_bw() +
-  ggtitle("PCA")
+  ggtitle("PCA") 
+
+dev.copy(png, paste0(outputPrefix, "_pca_colored.png"))
+dev.off()
 
 
-# specific genes that are contributing the most to pc1
-loading_scores <- pca$rotation[,1]
-gene_scores <- abs(loading_scores) ## get the magnitudes
-names(gene_scores) <- norm_counts$X
-gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
-top_10_genes <- names(gene_score_ranked[1:10])
-
-top_10_genes ## show the names of the top 10 genes
-
-pca$rotation[top_10_genes, 1] ## show the scores (and +/- sign)
-
-# lets see what everything looks like without con_3 and con_4 ------------------
-
-norm_counts <- select(norm_counts, -c("con_3", "con_4"))
-
-# re-run above PCA code
+# # specific genes that are contributing the most to pc1
+# loading_scores <- pca$rotation[,1]
+# gene_scores <- abs(loading_scores) ## get the magnitudes
+# names(gene_scores) <- norm_counts$X
+# gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
+# top_10_genes <- names(gene_score_ranked[1:10])
+# 
+# top_10_genes ## show the names of the top 10 genes
+# 
+# pca$rotation[top_10_genes, 1] ## show the scores (and +/- sign)
+# 
+# # lets see what everything looks like without con_3 and con_4 ------------------
+# 
+# norm_counts <- select(norm_counts, -c("con_3", "con_4"))
+# 
+# # re-run above PCA code
