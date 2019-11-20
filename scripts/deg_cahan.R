7033951d34a2576ed743e342bb7af11c4cbc17dd
tsoleary <tsoleary12@gmail.com>
2019-11-20 11:10:06 -0500

    some organizational changes

--- scripts/deg_cahan.R
+++ scripts/deg_cahan.R
@@ -1,7 +1,7 @@
 # Cahan Lab DEG ----------------------------------------------------------------
 
 require(dplyr)
-require(DESeq)
+require(DESeq2)
 require(ggpubr)
 require(ggplot2)
 require(VennDiagram)
@@ -14,10 +14,8 @@ prefix <- "Dm_cahan_deg"
 
 # Import in .counts files ------------------------------------------------------
 
-# working directory containg the .counts files
+# working directory containg the .counts files and metadata file
 setwd(directory_counts)
-
-# sample, group, filename information dataframe
 metadata <- read.csv("samples.txt")
 
 # read in each .cnt and compile a dataframe of read counts for each sample
@@ -25,7 +23,7 @@ count_df <- data.frame()
 
 for (i in 1:length(as.character(metadata$file))){
   t <- read.table(as.character(metadata$file)[i], header = TRUE)
-  t <- t[, c(1, length(t))]
+  t <- t[, c(1, length(t))] # use only the first and last column (gene & counts)
   colnames(t) <- c("gene", as.character(metadata$label)[i])
   if (i == 1){
     count_df <- t 
@@ -35,43 +33,45 @@ for (i in 1:length(as.character(metadata$file))){
 }
 
 # create .csv file to upload to PIVOT for DESeq normalization
-write.csv(count_df, "dm_seq_counts.csv", row.names = FALSE)
+write.csv(count_df, paste0(prefix, "_counts.csv"), row.names = FALSE)
 
-counts_table <- as.matrix(count_df[,2:length(count_df)])
-rownames(counts_table) <- count_df$gene
+# convert to a matrix and save for DESeqDataSetFromMatrix
+counts_mat <- as.matrix(count_df[,2:length(count_df)])
+rownames(counts_mat) <- count_df$gene
 
-# DESeq workflow in R ----------------------------------------------------------
-
-# Set the prefix for each output file name
-prefix <- "Dm_DESeq2"
+# DESeq set up -----------------------------------------------------------------
 
 treatments <- as.character(unique(metadata$condition))
 
-ddsHTSeq <- DESeqDataSetFromMatrix(countData = counts_table,
+ddsHTSeq <- DESeqDataSetFromMatrix(countData = counts_mat,
                                    colData = metadata,
                                    design = ~ condition)
 
 colData(ddsHTSeq)$condition <- factor(colData(ddsHTSeq)$condition,
                                       levels = treatments)
 
-# DESeq2 normalization ---------------------------------------------------------
+# DESeq2 normalization and differential gene expression analysis ---------------
+setwd(directory_results)
 
+# normalization
 dds <- DESeq(ddsHTSeq)
 
-# set the ctrl group as the control to be compared against
+# set the ctrl as the group to be compared against
 dds$condition <- relevel(dds$condition, ref = "ctrl")
 
+# write normalized results 
+write.table(as.data.frame(counts(dds, normalized = TRUE)), 
+            file = paste0(prefix, "_norm_counts.txt"), sep = '\t')
+
 # get the results of the differential expression
-res_all <- results(dds)
 res_hot <- results(dds, contrast = c("condition", "hot", "ctrl"))
 res_cold <- results(dds, contrast = c("condition", "cold", "ctrl"))
 
-
 # set data results and normalized reads to df
 resdata_hot <- merge(as.data.frame(res_hot), 
-                 as.data.frame(counts(dds, normalized = TRUE)), 
-                 by = "row.names", 
-                 sort = FALSE)
+                     as.data.frame(counts(dds, normalized = TRUE)), 
+                     by = "row.names", 
+                     sort = FALSE)
 names(resdata_hot)[1] <- "gene"
 
 resdata_cold <- merge(as.data.frame(res_cold), 
@@ -81,8 +81,6 @@ resdata_cold <- merge(as.data.frame(res_cold),
 names(resdata_cold)[1] <- "gene"
 
 # write results to file for each set of camparisons
-setwd(directory_results)
-
 write.csv(resdata_hot, 
           file = paste0(prefix, "_hot", "_results_with_norm.csv"), 
           row.names = FALSE)
@@ -90,14 +88,10 @@ write.csv(resdata_cold,
           file = paste0(prefix, "_cold", "_results_with_norm.csv"), 
           row.names = FALSE)
 
-# write normalized results 
-write.table(as.data.frame(counts(dds, normalized = TRUE)), 
-            file = paste0(prefix, "_norm_counts.txt"), sep = '\t')
-
 # MA plot of RNAseq data for entire dataset ------------------------------------
 
 # groups being compared as how it was saved in the result folder
-comp_cond <- "hot"
+comp_cond <- "cold"
 output_prefix <- paste(prefix, comp_cond, sep = "_")
 
 # load result file
@@ -106,7 +100,7 @@ csv_result_file <- list.files()[grepl(comp_cond, list.files())]
 res <- read.csv(csv_result_file)
 
 # create ma plot
-ggmaplot(res, main = comp_cond,
+ggmaplot(res, main = expression("4°C" %->% "25°C"),
          fdr = 0.05, fc = 2, size = 1,
          palette = c("#B31B21", "#1465AC", "#A6A6A680"),
          genenames = as.vector(res$gene),
@@ -118,7 +112,7 @@ ggmaplot(res, main = comp_cond,
          ggtheme = ggplot2::theme_minimal()) 
 
 setwd(directory_plots)
-dev.copy(png, paste0(prefix, "_MAplot_ggpubr_analysis.png"))
+dev.copy(png, paste0(output_prefix, "_MAplot_ggpubr_analysis.png"))
 dev.off()
 
 # Principle Componenet Analysis ------------------------------------------------
@@ -133,10 +127,7 @@ data.matrix <- as.matrix(norm_counts)
 data.matrix <- data.matrix[apply(data.matrix, 1, var) != 0, ]
 pca <- prcomp(t(data.matrix), scale = TRUE) 
 
-## plot pc1 and pc2
-plot(pca$x[,1], pca$x[,2])
-
-## make a scree plot
+# make a scree plot
 pca.var <- pca$sdev^2
 pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
 
@@ -176,41 +167,36 @@ setwd(directory_plots)
 dev.copy(png, paste0(prefix, "_pca_colored.png"))
 dev.off()
 
-
-
 # Venn diagram of DESeq results ------------------------------------------------
 
+# all differentially expressed genes
 hot_deg <- row.names(subset(res_hot, res_hot$padj <= 0.05))
-
 cold_deg <- row.names(subset(res_cold, res_cold$padj <= 0.05))
 
 setwd(directory_plots)
-
 venn.diagram(x = list(HOT = hot_deg, COLD = cold_deg),
              filename = paste0(prefix, "_venn_diagram.tiff"),
              lwd = 4, 
              fill = c("red", "blue"), 
              alpha = 0.5,
-             label.col = "white", 
-             cex = 3,
+             label.col = "black", 
+             cex = 1.7,
              fontfamily = "serif", 
              fontface = "bold",
              cat.col = c("red", "blue"), 
              cat.cex = 3, 
              cat.fontfamily = "serif",
              cat.fontface = "bold", 
-             cat.dist = c(0.03, 0.03), 
-             cat.pos = c(200, 180))
+             cat.dist = c(0.05, 0.03), 
+             cat.pos = c(220, 160))
 
-# subset to only genes that are significantly upregulated L2FC > 1.5
+# subset to only genes that are significantly upregulated, L2FC > 1.5
 hot_deg_up <- row.names(subset(res_hot, res_hot$padj <= 0.05 & 
                                res_hot$log2FoldChange > 1.5))
-
 cold_deg_up <- row.names(subset(res_cold, res_cold$padj <= 0.05 &
                                 res_cold$log2FoldChange > 1.5))
 
 setwd(directory_plots)
-
 venn.diagram(x = list(HOT = hot_deg_up, COLD = cold_deg_up),
              filename = paste0(prefix, "_up_reg_venn_diagram.tiff"),
              lwd = 4, 
@@ -226,4 +212,27 @@ venn.diagram(x = list(HOT = hot_deg_up, COLD = cold_deg_up),
              cat.fontface = "bold", 
              cat.dist = c(0.03, 0.03), 
              cat.pos = c(200, 180))
+
+# subset to only genes that are significantly downregulated, L2FC < -1.5
+hot_deg_down <- row.names(subset(res_hot, res_hot$padj <= 0.05 & 
+                                 res_hot$log2FoldChange < -1.5))
+cold_deg_down <- row.names(subset(res_cold, res_cold$padj <= 0.05 &
+                                  res_cold$log2FoldChange < -1.5))
+
+setwd(directory_plots)
+venn.diagram(x = list(HOT = hot_deg_down, COLD = cold_deg_down),
+             filename = paste0(prefix, "_down_reg_venn_diagram.tiff"),
+             lwd = 4, 
+             fill = c("red", "blue"), 
+             alpha = 0.5,
+             label.col = "black", 
+             cex = 1.5,
+             fontfamily = "serif", 
+             fontface = "bold",
+             cat.col = c("red", "blue"), 
+             cat.cex = 3, 
+             cat.fontfamily = "serif",
+             cat.fontface = "bold", 
+             cat.dist = c(0.05, 0.03), 
+             cat.pos = c(220, 160))
 
