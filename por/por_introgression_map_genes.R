2bb21dcb861870254642deb17314799df02d3de1
tsoleary <tsoleary12@gmail.com>
2020-09-03 12:49:04 -0400

    snps functions and annotation

--- por/por_introgression_map_genes.R
+++ por/por_introgression_map_genes.R
@@ -15,65 +15,28 @@ fst_sk <- read_csv(here::here("por/fst_SKxVT8_500_tidy.csv")) %>%
   mutate(start = BP - 250,
          end = BP + 250)
 
+# SNP level Fst table 
+fst_ch_snp <- read_csv(here::here("por/fst_CHxVT10_tidy.csv"))
+fst_sk_snp <- read_csv(here::here("por/fst_SKxVT8_tidy.csv"))
+
 
 # Download and save GTF from flybase ---
 # ftp://ftp.flybase.net/releases/FB2020_03/dmel_r6.34/gtf/
 x <- read_clean_gtf("~/Downloads/dmel-all-r6.34.gtf")
 
 
-# Function to match them up
-
-
-# ------------------------------------------------------------------------------
-# Function: gene_assoc_window
-# Description: Annotate with all genes associated with the specific window
-# Inputs: fst data frame and gtf data frame
-# Outputs: fst data frame with new gene_assoc column
-
-require(tidyverse)
-
-gene_assoc_window <- function(dat, gtf_dat) {
-  
-  dat$gene_assoc <- vector(mode = "character", length = nrow(dat))
-  
-  gtf_dat <- gtf_dat %>%
-    filter(feature == "gene")
-  
-  
-  for (i in 1:nrow(dat)){
-    print(i)
-    
-    # Find all genes that overlap the window in anyway
-    temp <- c( 
-      which(dat$CHR[i] == gtf_dat$chr & 
-              gtf_dat$end > dat$start[i] & 
-              gtf_dat$end < dat$end[i], TRUE),
-      which(dat$CHR[i] == gtf_dat$chr & 
-              gtf_dat$start > dat$start[i] & 
-              gtf_dat$start < dat$end[i], TRUE),
-      which(dat$CHR[i] == gtf_dat$chr & 
-              gtf_dat$start < dat$start[i] & 
-              gtf_dat$end > dat$end[i], TRUE)
-    )
-    
-    # Paste all gene symbols together with ; between. NA if there is none
-    if (is_empty(temp)){
-      dat$gene_assoc[i] <- NA
-    } else{
-      dat$gene_assoc[i] <- paste(unique(gtf_dat$gene_symbol[temp]), 
-                                 collapse = ";")
-    }
-    
-  }
-  return(dat)
-} 
-# End function -----------------------------------------------------------------
-
-
+# Annotate 500 bp windows with overlapping genes
 fst_ch <- gene_assoc_window(fst_ch, x)
 fst_sk <- gene_assoc_window(fst_sk, x)
 
+write_csv(fst_ch, here::here("por/fst_CHxVT10_500_tidy_gene_annot.csv"))
+write_csv(fst_sk, here::here("por/fst_SKxVT8_500_tidy_gene_annot.csv"))
 
 
 
+# Annotate snps with associated genes
+fst_ch_snp <- gene_assoc_window(fst_ch_snp, x)
+fst_sk_snp <- gene_assoc_window(fst_sk_snp, x)
 
+write_csv(fst_ch_snp, here::here("por/fst_CHxVT10_tidy_gene_annot.csv"))
+write_csv(fst_sk_snp, here::here("por/fst_SKxVT8_tidy_gene_annot.csv"))
