6879a99d168e3e30c86fc6cdf8a84de0d05aeb0d
tsoleary <tsoleary12@gmail.com>
2020-09-03 13:25:16 -0400

    pick top genes based on fst values then clean and save as txt to put in webgesault

--- por/por_introgression_map_genes.R
+++ por/por_introgression_map_genes.R
@@ -6,6 +6,7 @@
 
 # Load packages
 require(tidyverse)
+source(here::here("functions.R"))
 
 # Load the Fst tables ---
 fst_ch <- read_csv(here::here("por/fst_CHxVT10_500_tidy.csv")) %>%
@@ -24,7 +25,6 @@ fst_sk_snp <- read_csv(here::here("por/fst_SKxVT8_tidy.csv"))
 # ftp://ftp.flybase.net/releases/FB2020_03/dmel_r6.34/gtf/
 x <- read_clean_gtf("~/Downloads/dmel-all-r6.34.gtf")
 
-
 # Annotate 500 bp windows with overlapping genes
 fst_ch <- gene_assoc_window(fst_ch, x)
 fst_sk <- gene_assoc_window(fst_sk, x)
@@ -34,9 +34,39 @@ write_csv(fst_sk, here::here("por/fst_SKxVT8_500_tidy_gene_annot.csv"))
 
 
 
+
 # Annotate snps with associated genes
 fst_ch_snp <- gene_assoc_window(fst_ch_snp, x)
 fst_sk_snp <- gene_assoc_window(fst_sk_snp, x)
 
 write_csv(fst_ch_snp, here::here("por/fst_CHxVT10_tidy_gene_annot.csv"))
 write_csv(fst_sk_snp, here::here("por/fst_SKxVT8_tidy_gene_annot.csv"))
+
+
+# Pick outliers based on a specific comparison
+# Top fraction of outliers that you want to call significant (e.g. top 0.05)
+top_per <- 0.05
+
+fst_ch_top <- fst_ch %>%
+  filter(CHvCHF > quantile(CHvCHF, 1 - top_per)) %>%
+  filter(!is.na(gene_assoc))
+  
+
+# Clean up gene annotation column for downstream analysis, functional enrichment
+num_genes <- max(stringr::str_count(fst_ch_top$gene_assoc, pattern = ";") + 1)
+
+fst_ch_top <- fst_ch_top %>%
+  separate(gene_assoc, 
+           into = paste("gene", 1:num_genes, sep = "_"), 
+           sep = ";") %>%
+  pivot_longer(contains("gene_"), 
+               names_to = "lab", 
+               values_to = "gene",
+               values_drop_na = TRUE) %>% 
+  distinct(gene)
+
+# Save the 
+write_delim(fst_ch_top, 
+            here::here("por/fst_ch_genes_top_5_percent.txt"), 
+            delim = "/t")
+
