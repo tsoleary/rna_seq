9df1f116302f66ba9d2c9eb38995974399acccec
tsoleary <tsoleary12@gmail.com>
2020-09-03 11:06:53 -0400

    function correct to allow for the different types of overlap. but slow. trying to vectorize with purrr

--- por/por_introgression_map_genes.R
+++ por/por_introgression_map_genes.R
@@ -45,23 +45,57 @@ sum(fst_ch$start > x$start & fst_ch$CHR == x$chr)
 
 dat <- fst_ch
 
-dat$gene_assoc <- vector(mode = "character", length = nrow(dat))
-nrow(dat)
-for (i in 1:nrow(dat)){
-  chr <- dat$CHR[i]
-  start <- dat$start[i]
-  end <- dat$end[i]
+
+# ------------------------------------------------------------------------------
+# Function: gene_assoc_window
+# Description: Annotate with all genes associated with the specific window
+# Inputs: fst data frame and gtf data frame
+# Outputs: fst data frame with new gene_assoc column
+
+require(tidyverse)
+
+gene_assoc_window <- function(dat, gtf_dat) {
   
-  temp <- which(chr == x$chr & start > x$start & end < x$end, TRUE)
+  dat$gene_assoc <- vector(mode = "character", length = nrow(dat))
   
-  if (is_empty(temp)){
-    dat$gene_assoc[i] <- NA
-  } else{
-    dat$gene_assoc[i] <- paste(unique(x$gene_symbol[temp]), collapse = ";")
+  gtf_dat <- gtf_dat %>%
+    filter(feature == "gene")
+  
+  
+  for (i in 1:nrow(dat)){
+    print(i)
+    
+    # Find all genes that overlap the window in anyway
+    temp <- c( 
+      which(dat$CHR[i] == gtf_dat$chr & 
+              gtf_dat$end > dat$start[i] & 
+              gtf_dat$end < dat$end[i], TRUE),
+      which(dat$CHR[i] == gtf_dat$chr & 
+              gtf_dat$start > dat$start[i] & 
+              gtf_dat$start < dat$end[i], TRUE),
+      which(dat$CHR[i] == gtf_dat$chr & 
+              gtf_dat$start < dat$start[i] & 
+              gtf_dat$end > dat$end[i], TRUE)
+    )
+    
+    # Paste all gene symbols together with ; between. NA if there is none
+    if (is_empty(temp)){
+      dat$gene_assoc[i] <- NA
+    } else{
+      dat$gene_assoc[i] <- paste(unique(gtf_dat$gene_symbol[temp]), 
+                                 collapse = ";")
+    }
+    
   }
-  
+  return(dat)
+} 
+# End function -----------------------------------------------------------------
 
 
-}
+dat %>%
+  mutate(gene_assoc = map2_chr(dat))
+
+
+gene_assoc_window(fst_ch, x)
 
 
