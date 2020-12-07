afbdb60f7ffdd80969aeca78877d20ef0266f084
tsoleary <tsoleary12@gmail.com>
2020-12-07 14:56:24 -0500

    updated with new pairwise tests and sum of log combining

--- por/introgression_allele_stats.R
+++ por/introgression_allele_stats.R
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
 # Por's Introgression data - Stats on allele freqs bw replicates and VT
-# December 02, 2020
+# December 07, 2020
 # TS O'Leary
 # ------------------------------------------------------------------------------
 
@@ -10,111 +10,70 @@ source(here::here("por/por_funcs.R"))
 
 # Load data
 df_messy <- read_delim("~/Downloads/SKxVT8_3rep_rc", 
-                       delim = "\t") %>% head(1000)
-# Running this on the whole thing currently just tanks it. 
-# So I think either for looping each row individually or like a thousand 
-# 1000 row chunks make work
+                       delim = "\t")
 
+# Split it into chunks
+df <- chunkify(df_messy, r_chunk = 1000)
 
-# Tidy the data
-df <- df_messy %>%
-  # Pivot the data frame to long format to separate out type of allele 
-  # and count from total
-  pivot_longer(contains(c("_maj", "_min")), 
-               names_to = "geno_allele", 
-               values_to = "allele_frac") %>%
-  # Separate out the "genotype" from the allele typle
-  separate(geno_allele, 
-           into = c("geno", "major_minor"), 
-           sep = "_") %>%
-  # Remove SK parent bc we won't use it down stream
-  filter(geno != "SK") %>%
-  # Separate out the specific allele count from the total
-  separate(allele_frac, 
-           into = c("count", "total"), 
-           sep = "/") %>%
-  # Remove the total because we don't need it
-  select(-total) %>%
-  # Need to treat the counts as a numeric 
-  # (it is currently a character class bc the original "97/100" format)
-  mutate(count = as.numeric(count)) %>%
-  # Flip it back wider so that genotype, major, and minor allele counts have
-  # their own column each
-  pivot_wider(values_from = "count", 
-              names_from = "major_minor") %>%
-  # Nest all that in a data.frame so that each row is back to a single loci!
-  nest(allele_count_df = c(geno, maj, min))
-
-# Pooling allele counts across groups ------------------------------------------
-
-# Create another nested data.frame from the allele counts that pools 
-# all replicates together to be compared to the VT background
-df$allele_pooled_df <- map(df$allele_count_df, 
-                           pool_alleles_all)
-
-# Create another nested data.frame from the allele counts that pools 
-# all replicates of the specific reciprocal cross -- ditches the VT parental
-df$allele_rec_cross_pooled_df <- map(df$allele_count_df, 
-                                     pool_alleles_rec_cross)
-
-
-# Run the two-sided Fisher exact test on each of the different comparisons -----
-
-# Each individually
-df$fish_pval_all <- map_dbl(df$allele_count_df, 
-                            run_fisher)
-
-df$fish_pval_all <- map_dbl(df$allele_count_df, 
-                            run_fisher_MC)
-
-
-for (i in 1:40){
+# Tidy the data and create other pooled allele count nested dfs and run tests --
+for (i in 1:length(df)){
   print(i)
   tictoc::tic()
-  df$fisher_for_exact[i] <- run_fisher_exact(df$allele_count_df[[i]])
-  tictoc::toc()
-  tictoc::tic()
-  df$fisher_for_MC_10000[i] <- run_fisher_MC(df$allele_count_df[[i]], 
-                                         reps = 10000)
-  tictoc::toc()
-  tictoc::tic()
-  df$fisher_for_MC_100000[i] <- run_fisher_MC(df$allele_count_df[[i]], 
-                                         reps = 100000)
-  tictoc::toc()
-  tictoc::tic()
-  df$fisher_for_MC_1000000[i] <- run_fisher_MC(df$allele_count_df[[i]], 
-                                         reps = 1000000)
-  tictoc::toc()
-  tictoc::tic()
-  df$fisher_for_MC_10000000[i] <- run_fisher_MC(df$allele_count_df[[i]], 
-                                               reps = 10000000)
+  
+  # Tidy the data -----
+  df[[i]] <- tidy_allele_df(df[[i]])
+  
+  
+  # Pooling allele counts across groups -----
+  
+  # Create another nested data.frame from the allele counts that pools 
+  # all replicates together to be compared to the VT background
+  df[[i]]$allele_pooled_df <- map(df[[i]]$allele_count_df, 
+                                  pool_alleles_all)
+  
+  # Create another nested data.frame from the allele counts that pools 
+  # all replicates of the specific reciprocal cross -- ditches the VT parental
+  df[[i]]$allele_rec_cross_pooled_df <- map(df[[i]]$allele_count_df, 
+                                            pool_alleles_rec_cross)
+  
+  
+  # Run the statistical tests ----
+  
+  # Individual pairwise Fisher tests sum-of-logs combined p-value
+  df[[i]]$fish_pval_pw_comb <- map_dbl(df[[i]]$allele_count_df, 
+                                       run_fisher_pw_comb)
+  
+  # Pooled: hybrid v parental
+  df[[i]]$fish_pval_hy_par <- map_dbl(df[[i]]$allele_pooled_df, 
+                                      run_fisher_exact)
+  
+  # Pooled: reciprocal cross (e.g. SKF v VT8F)
+  df[[i]]$fish_pval_rcross <- map_dbl(df[[i]]$allele_rec_cross_pooled_df, 
+                                      run_fisher_exact)
+
   tictoc::toc()
 }
 
-x <- df[1:40, ] %>% 
-  select(chr, pos, contains("for_exact"), 21:24) %>%
-  pivot_longer(contains("for_"), 
-               names_to = "test", 
-               values_to = "pval") %>%
-  group_by(chr, pos) %>%
-  mutate(delta_pval = pval - pval[test == "fisher_for_exact"]) %>%
-  ungroup(chr, pos) %>%
-  group_by(test) %>%
-  summarize(sum_abs_delta_pval = sum(abs(delta_pval), na.rm = TRUE)) %>%
-  mutate(pval_ind_off = sum_abs_delta_pval/40)
+
+# Bind all rows back together
+df <- bind_rows(df)
 
 
-
-# Pooled: hybrid v parental
-df$fish_pval_hy_par <- map_dbl(df$allele_pooled_df, 
-                               run_fisher)
-
-# Pooled: reciprocal cross (e.g. SKF v VT8F)
-df$fish_pval_rcross <- map_dbl(df$allele_rec_cross_pooled_df, 
-                               run_fisher)
-
-write_delim(df %>% 
-              select(-contains("df")), 
-            here::here("por/results_temp_1000.txt"))
+# Adjust the p-values with Benjamini-Hochberg correction -----------------------
 
 
+df$fish_pval_pw_comb_padj <- p.adjust(df$fish_pval_pw_comb, 
+                                       method = "BH", 
+                                       n = nrow(df))
+
+df$fish_pval_hy_par_padj <- p.adjust(df$fish_pval_hy_par, 
+                                       method = "BH", 
+                                       n = nrow(df))
+
+df$fish_pval_rcross_padj <- p.adjust(df$fish_pval_rcross, 
+                                       method = "BH", 
+                                       n = nrow(df))
+
+
+# Save final output as a rds file -----
+saveRDS(df, "por_introg_df_fisher_whole_genome.rds")
--- por/por_funcs.R
+++ por/por_funcs.R
@@ -5,6 +5,58 @@
 # ------------------------------------------------------------------------------
 
 
+# ------------------------------------------------------------------------------
+# Function: chunkify
+# Description: split a data.frame to a list of data.frames of workable sizes
+# Inputs: the big data.frame and r_chunk is the rows per chunk
+# Outputs: list of data.frames
+
+chunkify <- function(df, r_chunk = 1000) {
+  
+  chunk  <- rep(1:ceiling(nrow(df)/r_chunk), each = r_chunk)[1:n]
+  
+  return(split(df, chunk))
+} 
+# End function -----------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Function: tidy_allele_df
+# Description: tidy the allele count df into nested df for fisher tests
+# Inputs: data.frame from Por's allele count output
+# Outputs: tidy nested data.frame for fisher tests
+
+tidy_allele_df <- function(dat) {
+  dat %>%
+    # Pivot the data frame to long format to separate out type of allele 
+    # and count from total
+    pivot_longer(contains(c("_maj", "_min")), 
+                 names_to = "geno_allele", 
+                 values_to = "allele_frac") %>%
+    # Separate out the "genotype" from the allele typle
+    separate(geno_allele, 
+             into = c("geno", "major_minor"), 
+             sep = "_") %>%
+    # Remove SK parent bc we won't use it down stream
+    filter(geno != "SK") %>%
+    # Separate out the specific allele count from the total
+    separate(allele_frac, 
+             into = c("count", "total"), 
+             sep = "/") %>%
+    # Remove the total because we don't need it
+    select(-total) %>%
+    # Need to treat the counts as a numeric 
+    # (it is currently a character class bc the original "97/100" format)
+    mutate(count = as.numeric(count)) %>%
+    # Flip it back wider so that genotype, major, and minor allele counts have
+    # their own column each
+    pivot_wider(values_from = "count", 
+                names_from = "major_minor") %>%
+    # Nest all that in a data.frame so that each row is back to a single loci!
+    nest(allele_count_df = c(geno, maj, min))
+} 
+# End function -----------------------------------------------------------------
+
 
 # ------------------------------------------------------------------------------
 # Function: pool_alleles_all
@@ -65,7 +117,7 @@ df_to_mat <- function(dat) {
 
 
 # ------------------------------------------------------------------------------
-# Function: run_fisher
+# Function: run_fisher_exact
 # Description: run fisher exact test on a data.frame 
 # Inputs: data.frame with a geno column
 # Outputs: matrix with geno as the rownames
@@ -96,4 +148,39 @@ run_fisher_MC <- function(dat, reps = 1000000) {
   
   return(p_val)
 }
+# End function -----------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------------
+# Function: run_fisher_pw_comb
+# Description: Run pairwise Fisher tests and combine BH corrected pvals with 
+#              the sum of logs method
+# Inputs: data.frame with geno column with the maj and min alleles, 
+#         and a background to compare it to
+# Outputs: single sum-of-logs combined p-value
+
+run_fisher_pw_comb <- function(dat, background = "VT8") {
+  
+  # Get the rows of the data frame that match and don't match the background 
+  # allele count that you are comparing each to
+  r_background <- which(dat$geno == background)
+  r_other <- which(dat$geno != background)
+  
+  # Initialize a vector to store the pairwise Fisher test p-values
+  pvals <- vector(mode = "numeric", length = length(r_other))
+  
+  # Run the Fisher test on all pairwise 
+  for (i in 1:length(r_other)) {
+    pvals[i] <- run_fisher_exact(dat[c(r_background, r_other[i]), ])
+  }
+  
+  # Adjust the p-values with Benjamini-Hochberg correction
+  fdr <- p.adjust(pvals, method = "BH", n = length(pvals))
+  
+  # Combine p-values with the sum of logs method
+  p_comb <- metap::sumlog(fdr)$p
+  
+  return(p_comb)
+} 
 # End function -----------------------------------------------------------------
